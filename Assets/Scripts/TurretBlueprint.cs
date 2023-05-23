86d2a08bf79525f2574d03b46f7adf35cc542b3d
emirYarici <e-yarici@hotmail.com>
2023-05-23 11:04:25 +0300

    Revert "Merge branch 'Emirhan'"
    This reverts commit 389258f4bbcb1503d96683fc22d9972baf0060b2, reversing
    changes made to 4e7e50f106f90e5f54b421265a8ce3f1cbf19fd6.

--- Assets/Scripts/TurretBlueprint.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[System.Serializable]
-public class TurretBlueprint
-{
-   public GameObject preFab;
-   public int cost;
-}
